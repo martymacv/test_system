59a42cf9b846792fdb5d424c25e92356cca450d8
mrmcmva <vsevolodmartynyuk@yandex.ru>
2024-06-18 20:32:04 +0300

    fast commit

--- Shell/create_db.sh
+++ Shell/create_db.sh
@@ -5,5 +5,6 @@ export SQL_DIR=/${HOME_DIR}/SQL
 export PGPASSFILE=/home/${USER}/.pgpass
 
 psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_shemas.sql
+psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_sequences.sql
 psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_tables.sql
 psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_procedures.sql
