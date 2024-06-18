c03d6672ad5314656f0ed4ea4397f3375d114ffe
mrmcmva <vsevolodmartynyuk@yandex.ru>
2024-06-18 19:59:36 +0300

    add to create db files

---
+++ Shell/create_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export HOME_DIR=/home/${USER}/Scripts
+export SQL_DIR=/${HOME_DIR}/SQL
+export PGPASSFILE=/home/${USER}/.pgpass
+
+psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_db.sql
+psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_shemas.sql
+psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_tables.sql
+psql -h 7.7.7.77 -p 5432 -d my_test_framework -U postgres -f "${SQL_DIR}"/create_procedures.sql
