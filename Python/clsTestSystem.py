62981a8bce909d5c98cd2112240776d6350ff21a
mrmcmva <vsevolodmartynyuk@yandex.ru>
2024-06-22 15:38:45 +0300

    add/upd files for work with test_system_config

--- Python/clsTestSystem.py
+++ Python/clsTestSystem.py
@@ -3,21 +3,61 @@ import json
 
 
 class TestSystemConf:
-    def __init__(self, path_to_json_config):
-        self.__path_to_json_config = path_to_json_config
+    def __init__(self, config=None):
+        self.__current_config = config
+
+    def read_from_json_config(self, path_to_json_config):
         with open(path_to_json_config, "r", encoding="utf-8") as f:
-            self.__actual_config = json.load(f)
-        self.__new_path_to_json_config = None
-        self.__new_config = None
+            self.__current_config = json.load(f)
+
+    def write_to_json_config(self, path_to_json_config):
+        with open(path_to_json_config, "w", encoding="utf-8") as f:
+            json.dump(self.__current_config, f, ensure_ascii=False, indent=2)
 
     def get_actual_test_config(self):
-        return self.__actual_config
+        return self.__current_config
 
-    def get_new_test_config(self):
-        return self.__new_config
+    def create_test_system_image(self,
+                                 uni_owner_list: set,
+                                 database_name_list: set,
+                                 tables_list: list[tuple],
+                                 authorization_list: list[tuple]):
+        for owner in uni_owner_list:
+            self.__current_config[owner] = dict()
+        for owner, table in tables_list:
+            self.__current_config[owner][table] = dict()
+            self.__current_config[owner][table]["full_check_list"] = []
+            self.__current_config[owner][table]["pass_check_list"] = []
+            self.__current_config[owner][table]["fail_check_list"] = []
+            self.__current_config[owner][table]["skip_check_list"] = []
+            self.__current_config[owner][table]["null_check_list"] = []
+            self.__current_config[owner][table]["quarantine_check_list"] = []
+            self.__current_config[owner][table]["other_check_list"] = []
+            self.__current_config["properties"] = dict()
+        for database_name in database_name_list:
+            self.__current_config["properties"][database_name] = dict()
+        for database_name, user, password in authorization_list:
+            self.__current_config["properties"][database_name][user] = ""
+            self.__current_config["properties"][database_name][password] = ""
+        return self.__current_config
 
-    def create_new_test_config(self, new_json_config):
-        self.__new_path_to_json_config = f"../CFG/test_config{datetime.datetime.now().strftime("%Y%m%d")}.json"
-        self.__new_config = new_json_config
-        with open(self.__new_path_to_json_config, "w", encoding="utf-8") as f:
-            json.dump(self.__new_config, f, ensure_ascii=False, indent=2)
+    def get_full_check_list(self, owner, table):
+        return self.__current_config[owner][table]["full_check_list"]
+
+    def get_pass_check_list(self, owner, table):
+        return self.__current_config[owner][table]["pass_check_list"]
+
+    def get_fail_check_list(self, owner, table):
+        return self.__current_config[owner][table]["fail_check_list"]
+
+    def get_skip_check_list(self, owner, table):
+        return self.__current_config[owner][table]["skip_check_list"]
+
+    def get_null_check_list(self, owner, table):
+        return self.__current_config[owner][table]["null_check_list"]
+
+    def get_quarantine_check_list(self, owner, table):
+        return self.__current_config[owner][table]["quarantine_check_list"]
+
+    def get_other_check_list(self, owner, table):
+        return self.__current_config[owner][table]["other_check_list"]
---
+++ Python/json_gen.py
@@ -0,0 +1,19 @@
+import json
+import subprocess
+import sys
+
+import clsTestSystem
+
+actual_json_config = subprocess.Popen(['bash', '../Shell/return_current_config.sh', 'test_config'], stdout=subprocess.PIPE).communicate()[:~0]
+
+# test_config = clsTestSystem.TestSystemConf(f"..CFG/test_config.json")
+test_config = clsTestSystem.TestSystemConf(actual_json_config)
+new_test_config = clsTestSystem.TestSystemConf()
+
+# return dict() from class clsTestSystem
+config = test_config.get_actual_test_config()
+
+new_config = new_test_config.create_test_system_image()
+
+new_test_config.write_to_json_config(new_config)
+new_config = test_config.get_actual_test_config()
